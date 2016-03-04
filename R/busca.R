9cca25cabee180e88aa5b2356fb52f6f77e4f415
Daniel Falbel <dfalbel@gmail.com>
2016-03-04 14:40:36 -0500

    novas funs

--- R/busca.R
+++ R/busca.R
@@ -87,5 +87,52 @@ baixar_pag_busca <- function(q, dir, t = 1){
     Sys.sleep(t)
   }
 
+  return(dir)
 }
+
+#' Tabelar página
+#'
+#' @param a arquivo html com uma página baixada
+#'
+#' @export
+tabelar_pagina <- function(a){
+  li <- xml2::read_html(a) %>%
+    rvest::html_nodes(".search-results-list") %>%
+    rvest::html_nodes("li")
+
+  url <- li %>%
+    rvest::html_nodes(".search-results-title") %>%
+    rvest::html_node("a") %>%
+    rvest::html_attr("href")
+
+  titulo <- li %>%
+    rvest::html_nodes(".search-results-title") %>%
+    rvest::html_node("a") %>%
+    rvest::html_text()
+
+  trecho <- li %>%
+    rvest::html_nodes(".content") %>%
+    rvest::html_text()
+
+  data.frame(
+    url = url,
+    titulo = titulo,
+    trecho = trecho,
+    stringsAsFactors = F
+  )
+}
+
+
+
+#' Tabelar págians de busca
+#'
+#' @param dir diretório com os arquivos das páginas de buscas baixadas.
+#'
+#' @export
+tabelar_busca <- function(dir){
+  a <- list.files(dir, full.names = T)
+  df <- plyr::ldply(a, tabelar_pagina)
+  return(df)
+}
+
 
